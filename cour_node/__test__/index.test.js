ebc38a8887940cd416df32c555edafe900424e6c
ayoub ouahid <aouahid123@gmail.com>
2023-10-22 11:59:07 +0200

    fix archi

--- cour_node/__test__/index.test.js
+++ cour_node/__test__/index.test.js
@@ -1,6 +1,6 @@
 //create a test for the route /pkm with jest
 const request = require('supertest');
-const app = require('../cour_node/index');
+const app = require('../index.js')
 
 describe('Test the /pkm path', () => {
     test('It should respond with status code 200', async () => {
